f912cf1d8ee93c375921e5bd99ceebeb6ee847a8
wang0630 <j2081499@gmail.com>
2018-08-06 16:29:25 +0000

    working

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_assets import Environment,Bundle
-from config import Config
+from .config import Config
 app = Flask(__name__,static_url_path='/static')
 app.config.from_object(Config)
 from app import route
