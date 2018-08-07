e9c5196857ae12221a82c4de847435e080872e9d
wang0630 <j2081499@gmail.com>
2018-08-07 16:19:29 +0800

    version2

--- app/route.py
+++ app/route.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from app import app
 from flask import render_template
-from .data import PTList,targetList,imglist
+from .data import PTList,targetList,imglist,bonelist
 from flask_assets import Environment,Bundle
 import os
 
@@ -15,3 +15,7 @@ def home():
 @app.route("/target")
 def target():
   return render_template("targetpage.html",targetList=targetList)
+
+@app.route("/boneMassMeasurement")
+def boneMassMeasurement():
+  return render_template("boneMassMeasurement.html",bonelist=bonelist)
