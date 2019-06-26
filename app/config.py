d6dedcd5b1098a7bc6d7b29c7d06fc5b06dfef84
wang0630 <j2081499@gmail.com>
2019-06-27 01:11:32 +0800

    Change config

--- app/config.py
+++ app/config.py
@@ -4,10 +4,10 @@ from pymongo import MongoClient
 class Config:
   APIKEY = os.environ.get("APIKEY")
   # For CSRF protection
-  SECRET_KEY = 'my fucking key'
+  SECRET_KEY = os.environ.get('SECRET_KEY')
   # db config
   DB_NAME = 'clinic'
-  MONGO_URL = "mongodb+srv://j2081499:s3353830@wang-clinic-ff80a.mongodb.net/test?retryWrites=true&w=majority"
+  MONGO_URL = f"mongodb+srv://{os.environ.get('MONGO_ACCOUNT')}:{os.environ.get('MONGO_PASSWORD')}@wang-clinic-ff80a.mongodb.net/test?retryWrites=true&w=majority"
   MONGO_DB = MongoClient(MONGO_URL)[DB_NAME]
   MONGO_COLLECTION_ADMINI = MONGO_DB['admini']
   MONGO_COLLECTION_ANNOUNCEMENT = MONGO_DB['announcement']
