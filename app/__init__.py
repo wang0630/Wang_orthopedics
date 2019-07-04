8ced58e573712b4a003e7711a3f7c1310e529858
wang0630 <j2081499@gmail.com>
2019-07-04 23:34:36 +0800

    Refactor head.html and add jinja2 path

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
+import jinja2
 from flask import Flask
 from flask_login import LoginManager
 from .config import Config
-# from flask_assets import Environment,Bundle
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
@@ -16,5 +16,12 @@ from .auth import auth
 # register blueprint
 app.register_blueprint(main)
 app.register_blueprint(auth)
+
+my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    # Load templates from app/template, so all html files can use relative path to refet to each other
+    jinja2.FileSystemLoader(['/app/template/']),
+  ])
+app.jinja_loader = my_loader
 
 from . import assets
