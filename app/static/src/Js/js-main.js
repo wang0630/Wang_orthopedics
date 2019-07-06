5cfe0721a03ae39d57aa7fa3c9c181966e22b0f0
wang0630 <j2081499@gmail.com>
2019-07-06 23:24:37 +0800

    Create an external js class for pagination

--- app/static/src/Js/js-main.js
+++ app/static/src/Js/js-main.js
@@ -1,70 +1,63 @@
 /* Pagination btn functionality */
 class Pagination {
-  constructor(ac_per_page) {
-    this.aList = document.getElementById("aList");
-    this.ac_per_page = ac_per_page;
+  constructor(announcements, ac_per_page) {
+    this._aList = document.getElementById("aList");
+    this._ac_per_page = ac_per_page;
+    this._announcements = announcements;
   }
 
-  createListItem(date, content) {
+  _createListItem(date, content) {
     return `<li class="announcement__list__item"> \
     <span class="announcement__list__item__date"> ${date} </span> \
     <p> ${content} </p> \
     </li>`;
   }
 
-  createListItems(currentPage) {
-    let start = currentPage * ac_per_page;
-    let end = start + ac_per_page > announcements.length ? announcements.length : start + ac_per_page;
+  _createListItems(currentPage) {
+    let start = currentPage * this._ac_per_page;
+    let end = start + this._ac_per_page > this._announcements.length ? this._announcements.length : start + this._ac_per_page;
     let listr = "";
-    console.log(end);
     for (let i = start; i < end; i++) {
-    // console.log(i);
-    // console.log(announcements);
-    listr = `${listr}${createListItem(announcements[i].date, announcements[i].content)}`;
+      // console.log(i);
+      // console.log(announcements);
+      listr = `${listr}${this._createListItem(this._announcements[i].date, this._announcements[i].content)}`;
     }
     return listr;
   }
 
+  _renderOnePage(num) {
+    const listr = this._createListItems(num);
+    this._aList.innerHTML = listr;
+  }
+
+  renderInit() {
+    window.addEventListener('load', () => {
+      this._renderOnePage(0);
+    });
+  }
+
+  addPagination() {
+    const paginations = Array.from(document.querySelectorAll(".announcement__pagination__item"));
+    /* 
+      Use arrow function
+      So this of this._renderOnePage will refer to the outer scope,
+      which is the callback function inside forEach(),
+      But the callback function inside forEach() is also an arrow function,
+      so the this keyword will refer to the outer scope,
+      which is addPagination(), and get the proper this binding
+    */
+
+    /* 
+      paginations.forEach(function(p) {
+        ......
+      }, this);
+
+      will also work since we pass this obj into forEach and bind the callback function to it
+    */
+    paginations.forEach((p) => {
+      p.addEventListener("click", () => {
+        this._renderOnePage(parseInt(p.innerHTML) - 1);
+      })
+    });
+  }
 }
-
-
-// Create one item
-const createListItem = (date, content) => `<li class="announcement__list__item"> \
-<span class="announcement__list__item__date"> ${date} </span> \
-<p> ${content} </p> \
-</li>`;
-// Pass in the currently active page number to create the proper announcements
-const createListItems = (currentPage) => {
-let start = currentPage * ac_per_page;
-let end = start + ac_per_page > announcements.length ? announcements.length : start + ac_per_page;
-let listr = "";
-console.log(end);
-for (let i = start; i < end; i++) {
-// console.log(i);
-// console.log(announcements);
-listr = `${listr}${createListItem(announcements[i].date, announcements[i].content)}`;
-}
-return listr;
-}
-// ------------ Load the first 4 announcements(if any) -------------
-window.addEventListener("load", () => {
-let aList = document.getElementById("aList");
-let listr = createListItems(0);
-// Insert the newly created element string
-aList.innerHTML = listr;
-});
-// ----------------------------------------------------------
-
-
-// ------- Add eventListener to every pagination btn ----------
-let paginations = Array.from(document.querySelectorAll(".announcement__pagination__item"));
-// console.log(paginations)
-paginations.forEach((p, index) => {
-p.addEventListener("click", () => {
-let aList = document.getElementById("aList");
-let listr = createListItems(parseInt(p.innerHTML) - 1);
-// Insert the newly created element string
-aList.innerHTML = listr;
-});
-})
-// ------------------------------------------------------------
