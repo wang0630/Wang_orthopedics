049c2805dc6f9b7677b13c73b00db76a8095149f
wang0630 <j2081499@gmail.com>
2019-07-07 15:48:51 +0800

    Refactor template structures, remove head.html

--- app/main.py
+++ app/main.py
@@ -15,15 +15,15 @@ def home():
   # Page the list per 4 announcements
   total_pages =  ceil(len(announcements) / AC_PER_PAGE)
   print(f'total_pages: {announcements}')
-  return render_template("home.html", ac_per_page=AC_PER_PAGE, announcements=announcements, total_pages=total_pages, PTList=PTList, imglist=imglist, metacontent=u"汪骨外科診所為大北門地區第一間骨外科診所，旨在提供病患最有效的骨外科治療．")
+  return render_template("home/home.html", ac_per_page=AC_PER_PAGE, announcements=announcements, total_pages=total_pages, PTList=PTList, imglist=imglist, metacontent=u"汪骨外科診所為大北門地區第一間骨外科診所，旨在提供病患最有效的骨外科治療．")
 
 @main.route("/target")
 def target():
-  return render_template("targetpage.html", targetList=targetList, metacontent=u"汪骨外科診所的主治項目．") 
+  return render_template("home/targetpage.html", targetList=targetList, metacontent=u"汪骨外科診所的主治項目．") 
 
 @main.route("/boneMassMeasurement")
 def boneMassMeasurement():
-  return render_template("boneMassMeasurement.html", bonelist=bonelist, metacontent=u"汪骨外科診所提供的骨質密度檢測．")
+  return render_template("home/boneMassMeasurement.html", bonelist=bonelist, metacontent=u"汪骨外科診所提供的骨質密度檢測．")
 
 @main.route("/google8029c7599236ab79.html")
 def comfirm():
