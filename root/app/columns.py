15d4387be7df3c42f2240dde797e751aa7dafa60
wang0630 <j2081499@gmail.com>
2019-09-28 13:42:53 +0800

    S3 and boto3
    1. Add boto3 in requirements.txt.
    
    2. Successfully upload img.

--- root/app/columns.py
+++ root/app/columns.py
@@ -1,6 +1,10 @@
+from os.path import join, dirname
+from uuid import uuid4
 from flask import render_template, request, redirect, url_for, Blueprint, current_app as app
 from flask_login import login_required
 from w3lib.url import parse_data_uri
+from boto3 import client
+from botocore.exceptions import ClientError
 from .data import input_info
 
 columns = Blueprint(name='columns', import_name=__name__ , url_prefix='/columns')
@@ -26,27 +30,60 @@ def get_one_column(id):
 # API
 @columns.route('', methods=['POST'])
 def create_column():
-  # Accept incoming msg
-  if (request.is_json):
-    # Create object
-    post = {
-      'author': request.json.get('author'),
-      'base64s': request.json.get('base64s'),
-      'content': request.json.get('content'),
-    }
-    try:
-      # Convert base64 img to real img
-      for base64 in post['base64s']:
-        result = parse_data_uri(base64)
-        print(result[0])
-    except ValueError:
-      return 'The img format is not allowed', 400
-
-    return '', 204
-  else:
-    return 'Request should be json', 400
-  
-  
+  try:
+    # Accept incoming msg
+    if (request.is_json):
+      # Create object
+      post = {
+        'author': request.json.get('author'),
+        'base64s': request.json.get('base64s'),
+        'content': request.json.get('content'),
+      }
+      try:
+        post_document = {
+          'author': request.json.get('author'),
+          'content': request.json.get('content'),
+          'imgurl': [],
+        }
+        if post['base64s']:
+          # Connect to S3
+          s3 = client(
+            's3',
+            aws_access_key_id=app.config['AWS_S3_KEY'],
+            aws_secret_access_key=app.config['AWS_S3_SECRET']
+          )
+          # Convert base64 img to real img
+          for base64 in post['base64s']:
+            result = parse_data_uri(base64)
+            if (result[0] == 'image/jpeg' or result[0] == 'image/png'):
+              new_id = uuid4()
+              extension = result[0][result[0].index('/') + 1:]
+              file_name = f'img/{new_id}.{extension}'
+              # Upload to aws S3
+              s3.put_object(
+                Body=result[2],
+                Bucket=app.config['BUCKET_COLUMN'],
+                Key=file_name,
+                ContentType=result[0],
+              )
+              post_document['imgurl'].append(file_name)
+            else:
+              raise ValueError('The img should be jpeg or png')
+          
+      except ValueError as e:
+        raise ValueError(e.args[0])
+      except ClientError as e:
+        print(e.args)
+        return e.args[0], 500
+      return '', 204
+    else:
+      return 'Request should be json', 406
+  except ValueError as e:
+    return e.args[0], 400
+  except KeyError as e:
+    return e.args[0], 500
+  except Exception as e:
+    return e.args[0], 500
   # upload to aws
   # return successful
 
--- root/app/config.py
+++ root/app/config.py
@@ -14,6 +14,10 @@ class Config:
   # Some constant
   AC_PER_PAGE = 4
   EDITOR_POST_LIMIT = 10
+  # AWS S3
+  BUCKET_COLUMN = 'wangcolumn'
+  AWS_S3_KEY = os.environ.get('AWS_S3_KEY')
+  AWS_S3_SECRET = os.environ.get('AWS_S3_SECRET')
   # Development
   ENV=os.environ.get('FLASK_ENV')
   REQUEST_IP=os.environ.get('REQUEST')
