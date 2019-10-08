93872fdfaa506993107a37fd23b8095380ad8856
wang0630 <j2081499@gmail.com>
2019-10-09 00:33:09 +0800

    Https support
    1. Open port 443 in production.
    
    2. Mount /etc/letsencrypt to access the cert and the key.
    
    3. https.conf is added, but cache is not configured yet.

--- root/app/columns.py
+++ root/app/columns.py
@@ -12,7 +12,7 @@ from .data import input_info
 from .dbService import fetch_columns_info
 from .dbService.helpers import insert_single_doc, get_collection_count, fetch_one_doc
 from helpers.html_multipulate import traverse_insert_img_src
-import helpers.cookie as cookie_lib
+# import helpers.cookie as cookie_lib
 
 columns = Blueprint(name='columns', import_name=__name__ , url_prefix='/columns')
 
--- root/app/helpers/cookie.py
+++ root/app/helpers/cookie.py
@@ -1,14 +1,15 @@
 from collections import deque
 
 def test_and_set_cookie(res, cookie, id):
-  cookie_queue = deque(cookie.split('&'), 5)
-  new_cookie = None
-  if id in cookie_queue:
-    pass
-  else:
-    if (len(cookie_queue) >= 5):
-      pass
-    else:
-      cookie_queue.append(id)
-      new_cookie = cookie_queue.join('&')
-  res.set_cookie('recent_view', new_cookie, max_age=86400)
+  # cookie_queue = deque(cookie.split('&'), 5)
+  # new_cookie = None
+  # if id in cookie_queue:
+  #   pass
+  # else:
+  #   if (len(cookie_queue) >= 5):
+  #     pass
+  #   else:
+  #     cookie_queue.append(id)
+  #     new_cookie = cookie_queue.join('&')
+  # res.set_cookie('recent_view', new_cookie, max_age=86400)
+  pass
