5e33a6f6b442b83f9aacd88bf1a0c272ed84c4c4
Ubuntu <ubuntu@ip-172-31-42-53.ap-northeast-1.compute.internal>
2019-10-10 09:30:09 +0000

    Push main.py

--- root/app/main.py
+++ root/app/main.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import render_template, request, redirect, url_for, Blueprint, current_app as app
+from flask import render_template, make_response, request, redirect, url_for, Blueprint, current_app as app
 from math import ceil
 from .data import PTList, targetList, imglist, bonelist
 
@@ -13,8 +13,9 @@ def home():
   announcements = list(app.config['MONGO_COLLECTION_ANNOUNCEMENT'].find({}, {'_id': False}).sort('date', -1))
   # Page the list per 4 announcements
   total_pages =  ceil(len(announcements) / AC_PER_PAGE)
-  # print(f'total_pages: {announcements}')
-  return render_template("home/home.html", ac_per_page=AC_PER_PAGE, announcements=announcements, total_pages=total_pages, PTList=PTList, imglist=imglist, metacontent=u"汪骨外科診所為大北門地區第一間骨外科診所，旨在提供病患最有效的骨外科治療．")
+  res = make_response(render_template("home/home.html", ac_per_page=AC_PER_PAGE, announcements=announcements, total_pages=total_pages, PTList=PTList, imglist=imglist, metacontent=u"汪骨外科診所為大北門地區第一間骨外科診所，旨在提供病患最有效的骨外科治療．"))
+  res.headers['Link'] = '</static/img/front.jpg>; as=image; rel=preload, </static/img/map.png>; as=image; rel=preload'
+  return res
 
 @main.route("/target")
 def target():
